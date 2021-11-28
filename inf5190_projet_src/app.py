1ed5200dfbb610142d5e0433fef97eddbb988d04
Carmont, Kelhyan <ea491048@ens.uqam.ca>
2021-11-27 23:36:15 -0500

    Chargement des données dans la BD

--- inf5190_projet_src/app.py
+++ inf5190_projet_src/app.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 from logging import error
 from flask import Flask
 from flask import render_template
@@ -5,43 +6,38 @@ from flask import g
 from flask import request
 from flask import redirect
 from flask.helpers import url_for
-from .db.database import Database
 import requests
 import xml.etree.ElementTree as ET
 import random
 import csv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from urllib import request as r
+
 
 app = Flask(__name__, static_url_path="", static_folder="static")
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Connexion à la BD
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        g._database = Database()
-    return g._database
+db = SQLAlchemy(app)
 
 
-# Fermeture de la connexion
+# Pas possible de mettre le model BD dans un module car
+# problème d'injection de dépendances entre BD, modèle et app
+class Activite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type_installation = db.Column(db.String, nullable=False)
+    nom = db.Column(db.String, nullable=False)
+    arrondissement = db.Column(db.String, nullable=False)
+    ajout_bd = db.Column(db.DateTime, default=datetime.now)
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.disconnect()
-
-
-# Route principale, s'il n'y a pas d'article dans la BD
-# retourne un message en conséquence pour en créer un
-# Sinon retourne au plus 5 article, par date décroissante
-
-@app.route("/")
-def accueil():
-    load_patinoires()
-    load_glissades()
-    load_piscines()
-    return render_template("index.html")
+    def __init__(self, id, type_installation, nom, arrondissement, ajout_bd):
+        self.id = id
+        self.type_installation = type_installation
+        self.nom = nom
+        self.arrondissement = arrondissement
+        self.ajout_bd = ajout_bd
 
 
 def load_patinoires():
@@ -51,15 +47,20 @@ def load_patinoires():
     root = ET.fromstring(req_patinoire.content)
     for arrondissement in root.findall('arrondissement'):
         id = random.randint(1, 30000)
-        nom_arr = arrondissement.find('nom_arr').text
+        nom_arr = str.strip(arrondissement.find('nom_arr').text)
         type = "patinoire"
-        nom = arrondissement.find('patinoire').find('nom_pat').text
-        # Ajout BD
+        nom = str.strip(arrondissement.find('patinoire').find('nom_pat').text)
+        act = Activite(id=id, type_installation=type,
+                       nom=nom, arrondissement=nom_arr, ajout_bd=datetime.now())
+        if(Activite.query.filter_by(nom=act.nom).first() is None):
+            print("N'existe pas dans BD : ajout")
+            db.session.add(act)
         print(id)
         print(nom_arr)
         print(type)
         print(nom)
-        print("----------")
+        print("============")
+    db.session.commit()
 
 
 def load_glissades():
@@ -72,31 +73,58 @@ def load_glissades():
         nom_arr = glissade.find('arrondissement').find('nom_arr').text
         type = "glissade"
         nom = glissade.find('nom').text
-        # Ajout BD
+        act = Activite(id=id, type_installation=type,
+                       nom=nom, arrondissement=nom_arr, ajout_bd=datetime.now())
+        if(Activite.query.filter_by(nom=act.nom).first() is None):
+            print("N'existe pas dans BD : ajout")
+            db.session.add(act)
         print(id)
         print(nom_arr)
         print(type)
         print(nom)
         print("----------")
+    db.session.commit()
 
 
 def load_piscines():
     print("Chargement des piscines : ")
-    with open("./data/piscines.csv", "r") as csv_piscine:
+    path = 'https://data.montreal.ca/dataset/4604afb7-a7c4-4626-a3ca-e136158133f2/resource/cbdca706-569e-4b4a-805d-9af73af03b14/download/piscines.csv'
+    dest = './data/piscines.csv'
+    r.urlretrieve(path, dest)
+    with open(dest, "r") as csv_piscine:
         next(csv_piscine)
         reader = csv.reader(csv_piscine, delimiter=',')
         for row in reader:
-            id = random.randint(
-                6000000, 9000000) if row[0] == "0" else int(row[0])
+            # Pour éviter de traiter les doublons dans ID du fichier
+            id = random.randint(6000000, 9999990)
             nom_arr = row[3]
             type = row[1]
             nom = row[2]
-            # Ajout BD
+            act = Activite(id=id, type_installation=type,
+                           nom=nom, arrondissement=nom_arr, ajout_bd=datetime.now())
+            if(Activite.query.filter_by(nom=act.nom).first() is None):
+                print("N'existe pas dans BD : ajout")
+                db.session.add(act)
             print(id)
             print(nom_arr)
             print(type)
             print(nom)
-            print("----------")
+            print("***********")
+        db.session.commit()
+
+
+if(isfile('db/database.db') == False):
+    print("Base de donnée inexistante : initialisation en cours...")
+    db.create_all()
+    load_patinoires()
+    load_glissades()
+    load_piscines()
+    print("Base de donnée correctement créée et chargée")
+
+
+@app.route("/")
+def accueil():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
--- inf5190_projet_src/db/db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE activite{
-    id INTEGER PRIMARY KEY,
-    type_installation TEXT,
-    nom TEXT,
-    arrondissement TEXT,
-}
